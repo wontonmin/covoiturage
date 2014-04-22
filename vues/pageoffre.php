a4c6295b28c1a699d2882576eb1d67d342ecdd94
wontonmin <brandonlee3006@gmail.com>
2014-04-23 01:29:09 +0200

    Ajout de la partie contact avec ses boutons.

--- vues/pageoffre.php
+++ vues/pageoffre.php
@@ -4,15 +4,14 @@ include "vues/entete_retour.html";
 ?>
  <div data-role="content" >  
     <div id="offre"> 
-        
-        Page en construction
+        <ul data-role ="listview" id="list" >
+            <li id="nom">                       </li>
+            <li id="prenom">                      </li>
+            <li id="ramassage">                  </li>  
+        </ul>
+        <a href="tel:numeroTel" data-role="button" data-icon="grid"   id="tel"  data-inline="true" data-mini="true">Appeler</a>
+        <a href="mail" data-role="button" data-inline="true" data-mini="true" id="mail">Contacter</a>
     </div>
-    
-    
-    
-    
-    
-    
  </div><!-- /content -->
 <?php
     include "vues/pied.html";
