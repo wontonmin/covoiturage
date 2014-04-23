926f6175ca9c506b20825dbb0921e08d33645ca0
wontonmin <brandonlee3006@gmail.com>
2014-04-24 01:18:03 +0200

    Modification boutons interface de connexion

--- vues/pageconnexion.php
+++ vues/pageconnexion.php
@@ -5,7 +5,7 @@ include "vues/logo.html";
 ?>
 <div data-role="content" id="divconnexion">       
    <!-- si on n'indique pas rel=external, charge tout le document -->
-   <a href="index.php?action=inscription" rel="external">Vous inscrire</a> 
+
     <div data-role="fieldcontain">
    
         <label for="login">Login </label>
@@ -16,7 +16,8 @@ include "vues/logo.html";
      </div>
    <div id="message" data-theme="e"></div>
      <p>
-       <a href="#" rel="external" data-role="button" id="btnconnexion" >Connexion</a>
+       <a href="#" rel="external" data-role="button" id="btnconnexion" data-mini="true" data-inline="true">Connexion</a>
+       <a href="index.php?action=inscription" data-role="button" rel="external" data-mini="true" data-inline="true">Vous inscrire</a> 
      </p>
         </div><!-- /content -->
         <div id=pied></div>
