d945e8baea8ce6ad5a05824d2e656d707d48d7be
lesamgames <Samuel@LeSam>
2014-04-23 18:41:00 -0300

    commentaire inutile

--- ajax/traitersuppression.php
+++ ajax/traitersuppression.php
@@ -1,9 +1,10 @@
 <?php
 require_once '../util/fonctions.php';
 
-$lesId =  $_REQUEST['lesIdOffres'];
+$lesId = $_REQUEST['lesIdOffres'];
 
 /* à faire dans une prochaine itération, en base de données*/
 // $lesId = supprimerOffres($lesId);
+
 echo json_encode($lesId);
 ?>
