af7ad10b6371fae9eb08935bb2f14bf4caebfe87
lesamgames <Samuel@LeSam>
2014-04-22 22:10:53 -0300

    Correction du boutton accueil

--- ajax/traiteroffre.php
+++ ajax/traiteroffre.php
@@ -2,16 +2,9 @@
 session_start();
 require_once '../util/fonctions.php';
 $idOffre= $_REQUEST['idOffre'];
-//$choix = $_SESSION['choix'];
-/*
-if($choix == 'arriveeentreprise')
-   $tabOffre = getOffreArriveeEntreprise($idOffre);
-else 
-   $tabOffre = getOffreDepartEntreprise($idOffre);
-   */ 
 
 $tabOffre = getOffreDepartEntreprise($idOffre);
-   
+
 echo json_encode($tabOffre);
 
 ?>
--- vues/pageoffre.php
+++ vues/pageoffre.php
@@ -2,12 +2,12 @@
 <?php
 include "vues/entete_retour.html";
 ?>
- <div data-role="content" >  
+ <div data-role="content">
     <div id="offre"> 
         <ul data-role ="listview" id="list" >
             <li id="nom">                        </li>
             <li id="prenom">                     </li>
-            <li id="ramassage">                  </li>  
+            <li id="ramassage">                  </li>
         </ul>
         <a href="tel:numeroTel" data-role="button" data-icon="grid" id="tel" data-inline="true" data-mini="true">Appeler</a>
         <a href="mail" data-role="button" data-inline="true" data-mini="true" id="mail">Contacter</a>
