dc0a4a53844fe0142dfd681f7bbaac4527c9804a
lesamgames <Samuel@LeSam>
2014-05-01 22:24:23 -0300

    ajout connexion script

--- connexionbdd.php
+++ connexionbdd.php
@@ -1,4 +1,10 @@
 <?php
-    $bdd = new PDO('mysql:host=localhost;dbname=covoiturage', 'root', '');
+
+    $host = "localhost";
+    $base = "covoiturage";
+    $utilisateur = "root";
+    $pass = "";
+
+    $bdd = new PDO('mysql:host=$host;dbname=$base', $utilisateur, $pass);
     
 ?>
